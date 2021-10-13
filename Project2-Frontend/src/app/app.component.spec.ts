71dda7acec7a4e1132d5b979c232239156d763cb
Evan Coye <ecoye3412@gmail.com>
2021-10-13 09:48:22 -0400

    Second Try

--- Project2-Frontend/src/app/app.component.spec.ts
+++ Project2-Frontend/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'recipe-book'`, () => {
+  it(`should have as title 'Project2-Frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('recipe-book');
+    expect(app.title).toEqual('Project2-Frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('recipe-book app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Project2-Frontend app is running!');
   });
 });
