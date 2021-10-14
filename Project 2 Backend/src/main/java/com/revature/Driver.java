d8a47096a010b13845a9e9b78016a8fa5036ad1a
Andreas Kustas <atkustas@gmail.com>
2021-10-14 16:17:29 -0400

    added Hibernate to model classes, put Javalin in Driver

--- Project 2 Backend/src/main/java/com/revature/Driver.java
+++ Project 2 Backend/src/main/java/com/revature/Driver.java
@@ -1,9 +1,35 @@
 package com.revature;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
+import com.revature.utils.HibernateUtil;
+
+import io.javalin.Javalin;
+
 public class Driver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("%%%%% MAIN LOADED %%%%%");
+		
+		//open connection
+		try (Session ses = HibernateUtil.getSession()) {
+
+			System.out.println("Hello you have a Connection to your DB with Hibernate!");
+			HibernateUtil.closeSession();// could leave this connection test in, close the session within the try/catch
+		} catch (HibernateException e) {
+			System.out.println("DB connection has failed!!");
+		}
+
+		
+		//get server up
+		Javalin app = Javalin.create(
+
+				config -> {
+					config.enableCorsForAllOrigins();// allows the sever to process JS request from anywhere
+				}
+
+		).start(8090);
 
 	}
 
--- Project 2 Backend/src/main/java/com/revature/models/User.java
+++ Project 2 Backend/src/main/java/com/revature/models/User.java
@@ -1,9 +1,22 @@
 package com.revature.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class User {
 	
 	//variables
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id")
 	private int user_id;
+	
 	private String email;
 	private String username;
 	private String password;
