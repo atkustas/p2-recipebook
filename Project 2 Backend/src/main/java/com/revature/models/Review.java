69df924136725df2ebdc6f39e270e431456f5fe1
Andreas Kustas <atkustas@gmail.com>
2021-10-13 22:19:25 -0400

    added date to Review

--- Project 2 Backend/src/main/java/com/revature/models/Review.java
+++ Project 2 Backend/src/main/java/com/revature/models/Review.java
@@ -6,6 +6,7 @@ public class Review {
 	private int user_id;
 	private int drink_id;
 	private String review;
+	private String date;
 	
 	public Review() {
 		super();
@@ -13,32 +14,75 @@ public class Review {
 	}
 
 	//all-args
-	public Review(int rev_id, int user_id, int drink_id, String review) {
+	public Review(int rev_id, int user_id, int drink_id, String review, String date) {
 		super();
 		this.rev_id = rev_id;
 		this.user_id = user_id;
 		this.drink_id = drink_id;
 		this.review = review;
+		this.date = date;
 	}
 
 	//no-id
-	public Review(int user_id, int drink_id, String review) {
+	public Review(int user_id, int drink_id, String review, String date) {
 		super();
 		this.user_id = user_id;
 		this.drink_id = drink_id;
 		this.review = review;
+		this.date = date;
 	}
 
 	@Override
 	public String toString() {
 		return "Review [rev_id=" + rev_id + ", user_id=" + user_id + ", drink_id=" + drink_id + ", review=" + review
-				+ "]";
+				+ ", date=" + date + "]";
+	}
+
+	public int getRev_id() {
+		return rev_id;
+	}
+
+	public void setRev_id(int rev_id) {
+		this.rev_id = rev_id;
+	}
+
+	public int getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+
+	public int getDrink_id() {
+		return drink_id;
+	}
+
+	public void setDrink_id(int drink_id) {
+		this.drink_id = drink_id;
+	}
+
+	public String getReview() {
+		return review;
+	}
+
+	public void setReview(String review) {
+		this.review = review;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + ((date == null) ? 0 : date.hashCode());
 		result = prime * result + drink_id;
 		result = prime * result + rev_id;
 		result = prime * result + ((review == null) ? 0 : review.hashCode());
@@ -55,6 +99,11 @@ public class Review {
 		if (getClass() != obj.getClass())
 			return false;
 		Review other = (Review) obj;
+		if (date == null) {
+			if (other.date != null)
+				return false;
+		} else if (!date.equals(other.date))
+			return false;
 		if (drink_id != other.drink_id)
 			return false;
 		if (rev_id != other.rev_id)
@@ -68,6 +117,10 @@ public class Review {
 			return false;
 		return true;
 	}
+	
+	
+	
+	
 	
 	
 
