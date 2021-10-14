40be390fcbdf7f3869775bfb63df614c8bf0dbbd
Evan Coye <ecoye3412@gmail.com>
2021-10-13 20:08:35 -0400

    Deleted some extra configuration files and addressed the routing error

--- Project2-Frontend/src/app/app.module.ts
+++ Project2-Frontend/src/app/app.module.ts
@@ -4,13 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CocktailComponentsComponent } from './cocktail-components/cocktail-components.component';
+import { NavbarComponent } from './cocktail-components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CocktailComponent,
     CocktailComponentsComponent,
-    NewCocktailComponent
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
