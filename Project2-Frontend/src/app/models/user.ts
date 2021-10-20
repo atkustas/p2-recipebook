98ff5597c1e1e257e83422901416d1e77547b080
Evan Coye <ecoye3412@gmail.com>
2021-10-20 17:51:13 -0400

    Added the models needed to connect to the users and reviews tables on the backend

--- Project2-Frontend/src/app/models/user.ts
+++ Project2-Frontend/src/app/models/user.ts
@@ -1,2 +1,11 @@
 export class User {
+    
+    constructor(
+        private dob:String,
+        private email:String,
+        private firstname:String,
+        private lastname:String,
+        private password:String,
+        private username:String
+    ){}
 }
