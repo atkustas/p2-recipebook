cb040ee38d0fcf8961498f73ae71942546b0f0d8
Freddie-TadeoL <geeps_1999@live.com>
2021-10-26 14:18:50 -0700

    registration now making new users with a fetch

--- Project 2 Backend/src/main/java/com/revature/Driver.java
+++ Project 2 Backend/src/main/java/com/revature/Driver.java
@@ -34,7 +34,7 @@ public class Driver {
 		ReviewDao rd = new ReviewDao();
 		ReviewController rvc = new ReviewController();
 		RegistrationController rc = new RegistrationController();
-		//FavoriteController fc = new FavoriteController();
+		FavoriteController fc = new FavoriteController();
 		
 		UserDao uDao = new UserDao();
 		FavoriteDao fd = new FavoriteDao();
@@ -62,9 +62,9 @@ public class Driver {
 		app.post("/register", rc.register);
 		
 		app.post("/addcocktail", cc.addcocktail);
-//		app.post("/addfavorite", fc.addfavorite);
-//		app.post("/addfav", fc.addfav);
-//		app.post("/viewfavorites", fc.userfavs);
+		app.post("/addfavorite", fc.addfavorite);
+		app.post("/addfav", fc.addfav);
+		app.post("/viewfavorites", fc.userfavs);
 		app.post("/addreview", rvc.addreview);
 		app.get("/allreviews", rvc.getallreviews);
 		app.post("/reviewsbyuser", rvc.reviewsbyuser);
