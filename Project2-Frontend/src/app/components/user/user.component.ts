48a21d9dc49d85028899aafa8afae28067ad221c
atkustas <90417477+atkustas@users.noreply.github.com>
2021-10-27 22:50:22 -0400

    last Testing commit

--- Project2-Frontend/src/app/components/user/user.component.ts
+++ Project2-Frontend/src/app/components/user/user.component.ts
@@ -1,11 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
 import { LoginService } from 'src/app/services/login.service';
 import { FavoriteService } from 'src/app/services/favorite.service';
 import { ReviewService } from 'src/app/services/review.service';
 import { CocktailService } from 'src/app/services/cocktail.service';
-=======
->>>>>>> master
 
 @Component({
   selector: 'app-user',
@@ -14,7 +11,6 @@ import { CocktailService } from 'src/app/services/cocktail.service';
 })
 export class UserComponent implements OnInit {
 
-<<<<<<< HEAD
   constructor(private ls:LoginService, private fs: FavoriteService, private rs: ReviewService,
     private cs:CocktailService) { }
 
@@ -150,11 +146,4 @@ export class UserComponent implements OnInit {
   }
 
 
-=======
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
->>>>>>> master
 }
