4047abdef5d42b0c4247813f094789732e2a3510
atkustas <90417477+atkustas@users.noreply.github.com>
2021-10-28 13:15:27 -0400

    login functionality fixed

--- Project 2 Backend/src/main/java/com/revature/services/LoginService.java
+++ Project 2 Backend/src/main/java/com/revature/services/LoginService.java
@@ -5,6 +5,29 @@ import com.revature.daos.UserDao;
 public class LoginService {
 
 	UserDao uDao = new UserDao();
+<<<<<<< HEAD
+=======
+	
+	public boolean login(String username, String password) {
+		
+		//check db for user based on username
+		User user = uDao.userLogin(username);
+		
+		//check that username & password provided match credentials in db
+		Boolean areEqual = username.equals(user.getUsername());
+		Boolean areEqual2 = username.equals(user.getPassword());
+		
+		//if the credentials match, return true
+		if(areEqual && areEqual2) {
+			return true;
+		}
+		
+		//if the credentials don't match, return false
+		return false;
+		
+	}
+	
+>>>>>>> parent of a12cc23a7 (login functionality fixed)
 
 	public boolean login(String username, String password) {
 		return uDao.getCredentials(username, password);
